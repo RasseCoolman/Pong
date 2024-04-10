d3844a151e79f27f6044a1719fa36368f5ea12d7
RasseCoolman <146823487+RasseCoolman@users.noreply.github.com>
2024-04-10 15:14:49 +0200

    wdq
    qwfer

--- Pong Game - Alvin x Rasmus/Assets/BallMovement.cs
+++ Pong Game - Alvin x Rasmus/Assets/BallMovement.cs
@@ -7,7 +7,7 @@ public class BallMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
